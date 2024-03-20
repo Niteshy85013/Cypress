df7bde491492ac67f262b4ba02567ee5e8187c7e
Nitesh yadav <niteshyadav85013@gmail.com>
2024-03-20 16:19:54 +0545

    updated

--- cypress/e2e/Automation_Testing_Practice/InputFields.cy.js
+++ cypress/e2e/Automation_Testing_Practice/InputFields.cy.js
@@ -1,10 +1,15 @@
+import { faker } from "@faker-js/faker"
 describe('Input Fields', ()=>{
     it.only('Checking for Inputfields',()=>{
         cy.visit('https://testautomationpractice.blogspot.com/')
 
-        cy.get('#name').type('Nitesh')
-        cy.get('#email').type('nitesh@gmail.com')
-        cy.get('#phone').type('9800000000')
+         const name = faker.person.firstName()
+         const email = faker.internet.email()
+         const phone = faker.phone.number()
+
+        cy.get('#name').type(name)
+        cy.get('#email').type(email)
+        cy.get('#phone').type(phone)
         cy.get('#textarea').type('This is a textarea field')
 
         // Assertion Title
@@ -40,6 +45,10 @@ describe('Input Fields', ()=>{
         cy.get('#colors').select('Green')
         // Assertion
         cy.get('[value="green"]').should('have.value', 'green');
+
+
+        // DatePicker
+        cy.get('#datepicker').type('2024-03-19')
         
 
     })
