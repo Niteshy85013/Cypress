f7aab4645f77ba87cef6096ddea9b8926d891e00
Nitesh yadav <niteshyadav85013@gmail.com>
2024-04-12 16:17:47 +0545

    iFrame Updated

--- cypress/e2e/Automation_Testing_Practice/InputFields.cy.js
+++ cypress/e2e/Automation_Testing_Practice/InputFields.cy.js
@@ -2,9 +2,11 @@ import { faker } from "@faker-js/faker";
 import "cypress-iframe";
 
 describe("Input Fields", () => {
-  it("Checking for Inputfields", () => {
+  beforeEach(() => {
     cy.visit("https://testautomationpractice.blogspot.com/");
+  });
 
+  it("Checking for Inputfields", () => {
     const name = faker.person.firstName();
     const email = faker.internet.email();
     const phone = faker.phone.number();
@@ -13,11 +15,6 @@ describe("Input Fields", () => {
     cy.get("#email").type(email);
     cy.get("#phone").type(phone);
     cy.get("#textarea").type("This is a textarea field");
-
-    // Assertion Title
-    //cy.get('.title').should('have.text', 'Automation Testing Practice');
-    //cy.get(':nth-child(45) > [data-layer="Content"]').should('have.text', 'Automation Testing Practice');
-    //cy.contains(':nth-child(45) > [data-layer="Content"]','Automation Testing Practice').should('be.visible')
 
     // For Radio buttons
     cy.get('input[value="male"]').check();
@@ -90,35 +87,30 @@ describe("Input Fields", () => {
     cy.get("#HTML10 > .widget-content > button").dblclick();
   });
 
-  it.only("iFrame", () => {
-    cy.visit("https://testautomationpractice.blogspot.com/");
+  it.only("iFrame Testing", () => {
+    cy.get("#HTML6").scrollIntoView({ duration: 2000 });
 
-    // Title Assertion
+    cy.frameLoaded("#frame-one796456169").then(($iframe) => {
+      const $body = $iframe.contents().find("body");
 
-    // Input Field
-    cy.iframe().find("#RESULT_TextField-0").type("Dummy Text");
+      // Input Field
+      cy.wrap($body).find("#RESULT_TextField-0").type("Dummy Text");
 
-    // Gender
-    cy.iframe()
-      .find('input[value="Radio-0"]')
-      .invoke("prop", "checked", true)
-      .should("be.checked");
+      // Radio button
+      cy.wrap($body)
+        .find('input[value="Radio-0"]')
+        .invoke("prop", "checked", true)
+        .should("be.checked");
 
-    // Date
-    //cy.iframe().find("#RESULT_TextField-2").type("04/11/2024");
+      // Random Date (Getting from custom command)
+      cy.wrap($body).find("#RESULT_TextField-2").type("04/11/2024");
 
-    // Date
-    cy.iframe()
-      .should("be.visible")
-      .then((iframe) => {
-        iframe.find(".icon_calendar").click();
-        iframe.find('.ui-datepicker-week-end[selectDay="11"]').click();
-      });
+      // Dropdown
+      cy.wrap($body).find(".drop_down").select("QA Engineer");
+      cy.wait(2000);
 
-    // Dropdown
-    //cy.iframe().find(".drop_down").select("QA Engineer");
-
-    // Submit Button
-    //cy.iframe().find("#FSsubmit").should("be.visible").click();
+      // Submit Button
+      //cy.wrap($body).find("#FSsubmit").should("be.visible").click();
+    });
   });
 });
